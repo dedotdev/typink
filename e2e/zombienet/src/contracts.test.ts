78704d425e536f67ab17d23c0853089bd8ee4c4b
Thang X. Vu <thang@coongcrafts.io>
2024-11-24 02:10:28 +0700

    test client connection

--- e2e/zombienet/src/contracts.test.ts
+++ e2e/zombienet/src/contracts.test.ts
@@ -1,7 +1,12 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expectTypeOf, it } from 'vitest';
+import { DedotClient, WsProvider } from 'dedot';
+
+const client = await DedotClient.new(new WsProvider('http://localhost:9944'));
 
 describe('Contracts', () => {
-  it('should ', () => {
-    expect(1 + 2).toEqual(3);
+  it('should get current block number', async () => {
+    const blockNumber = await client.query.system.number();
+    console.log('Current block number:', blockNumber);
+    expectTypeOf(blockNumber).toBeNumber();
   });
 });
