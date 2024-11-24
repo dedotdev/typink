afe31dffb1fce84932ebaffbac12affe31156bb3
Thang X. Vu <thang@coongcrafts.io>
2024-11-24 12:37:05 +0700

    basic tests

--- e2e/zombienet/src/basic.test.ts
+++ e2e/zombienet/src/basic.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, expectTypeOf, it } from 'vitest';
+import { afterAll, beforeAll, describe, expect, expectTypeOf, it } from 'vitest';
 import { DedotClient, WsProvider } from 'dedot';
 
 let client: DedotClient;
@@ -11,6 +11,14 @@ describe('Contracts', () => {
     const blockNumber = await client.query.system.number();
     console.log('Current block number:', blockNumber);
     expectTypeOf(blockNumber).toBeNumber();
+  });
+
+  it('should fetch account balance', async () => {
+    const account = await client.query.system.account('5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY');
+
+    console.log('Account balance:', account);
+
+    expect(account.data.free).toEqual(10000000000000000n);
   });
 });
 
