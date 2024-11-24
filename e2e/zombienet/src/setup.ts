ac38a1493fedf7e6ce10aa36fd825ac4fd0396d2
Thang X. Vu <thang@coongcrafts.io>
2024-11-24 13:41:11 +0700

    make sure ALICE & BOB balances are different

--- e2e/zombienet/src/setup.ts
+++ e2e/zombienet/src/setup.ts
@@ -1,11 +1,29 @@
 import { afterAll, beforeAll } from 'vitest';
 import { DedotClient, WsProvider } from 'dedot';
+import { Keyring } from '@polkadot/keyring';
+import { cryptoWaitReady } from '@polkadot/util-crypto';
+import { BOB } from 'utils';
 
 const CONTRACTS_NODE_ENDPOINT = 'ws://127.0.0.1:9944';
 
 beforeAll(async () => {
   console.log(`Connect to ${CONTRACTS_NODE_ENDPOINT}`);
-  global.client = await DedotClient.new(new WsProvider(CONTRACTS_NODE_ENDPOINT));
+  const client = await DedotClient.new(new WsProvider(CONTRACTS_NODE_ENDPOINT));
+
+  await cryptoWaitReady();
+
+  const aliceKeypair = new Keyring({ type: 'sr25519' }).addFromUri('//Alice');
+
+  // just to make sure ALICE & BOB balances are different!
+  await new Promise<void>((resolve) => {
+    client.tx.balances.transferKeepAlive(BOB, 1_000_000_000_000n).signAndSend(aliceKeypair, ({ status }) => {
+      if (status.type === 'BestChainBlockIncluded' || status.type === 'Finalized') {
+        resolve();
+      }
+    });
+  });
+
+  global.client = client;
 });
 
 afterAll(async () => {
