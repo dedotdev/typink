674f667200e0a5f29a4c005c5cdec30a2316b7ee
Thang X. Vu <thang@coongcrafts.io>
2024-11-24 12:49:29 +0700

    global client setup

--- e2e/zombienet/src/basic.test.ts
+++ e2e/zombienet/src/basic.test.ts
@@ -1,12 +1,6 @@
-import { afterAll, beforeAll, describe, expect, expectTypeOf, it } from 'vitest';
-import { DedotClient, WsProvider } from 'dedot';
+import { describe, expect, expectTypeOf, it } from 'vitest';
 
-let client: DedotClient;
-beforeAll(async () => {
-  client = await DedotClient.new(new WsProvider('ws://127.0.0.1:9944'));
-});
-
-describe('Contracts', () => {
+describe('basic client operations', () => {
   it('should get current block number', async () => {
     const blockNumber = await client.query.system.number();
     console.log('Current block number:', blockNumber);
@@ -18,10 +12,6 @@ describe('Contracts', () => {
 
     console.log('Account balance:', account);
 
-    expect(account.data.free).toEqual(10000000000000000n);
+    expect(account.data.free).toBeGreaterThan(0n);
   });
-});
-
-afterAll(async () => {
-  await client.disconnect();
 });
