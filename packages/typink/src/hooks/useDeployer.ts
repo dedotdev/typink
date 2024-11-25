93d1e0c871bb833f39f5ac084d3f4e08a5a21dfb
Shouse <aowiej23s@gmail.com>
2024-11-26 00:15:48 +0700

    feat: add deployer hooks

---
+++ packages/typink/src/hooks/useDeployer.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { useTypink } from './useTypink.js';
+import { ContractDeployer, ExecutionOptions, GenericContractApi } from 'dedot/contracts';
+import { ContractMetadata } from '@dedot/contracts/types';
+import { Hash } from '@dedot/codecs';
+
+export type UseDeployer<T extends GenericContractApi = GenericContractApi> = {
+  deployer?: ContractDeployer<T>;
+  // TODO add list of exposed constructors from the metadata
+};
+
+export function useDeployer<T extends GenericContractApi = GenericContractApi>(
+  metadata: ContractMetadata | string,
+  codeHashOrWasm: Hash | Uint8Array | string,
+  options: ExecutionOptions = {},
+): UseDeployer<T> {
+  const { client, networkId, connectedAccount, defaultCaller } = useTypink();
+  const [deployer, setDeployer] = useState<ContractDeployer<T>>();
+
+  useEffect(() => {
+    if (!client || !networkId) {
+      setDeployer(undefined);
+      return;
+    }
+
+    const deployer = new ContractDeployer<T>(
+      client,
+      metadata,
+      codeHashOrWasm, // prettier-end-here
+      {
+        defaultCaller: connectedAccount?.address || defaultCaller,
+        ...options,
+      },
+    );
+
+    setDeployer(deployer);
+  }, [client, networkId, connectedAccount?.address, defaultCaller]);
+
+  return {
+    deployer,
+  };
+}
