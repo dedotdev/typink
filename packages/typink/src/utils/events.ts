1b7767aad4058355581a9157e768b85b2ee6b38f
1cedrus <aowiej23s@gmail.com>
2024-12-30 23:49:00 +0700

    refactor: use EventEmitter from @dedot/utils

--- packages/typink/src/utils/events.ts
+++ packages/typink/src/utils/events.ts
@@ -1,4 +1,4 @@
-import { EventEmitter as EE } from 'eventemitter3';
+import { EventEmitter } from 'dedot/utils'
 import type { FrameSystemEventRecord } from 'dedot/chaintypes';
 
 export type SystemEventsListener = (events: FrameSystemEventRecord[]) => void;
@@ -6,76 +6,6 @@ export type Unsub = () => void;
 export type HandlerFn = (...args: any[]) => void;
 export enum InternalEvent {
   SystemEvents = 'SYSTEM_EVENTS',
-}
-
-export interface IEventEmitter<EventTypes extends string = string> {
-  on(event: EventTypes, handler: HandlerFn): Unsub;
-  once(event: EventTypes, handler: HandlerFn): Unsub;
-  off(event: EventTypes, handler?: HandlerFn): this;
-}
-
-const handlerWrapper = (handler: HandlerFn): HandlerFn => {
-  return (...args: any[]) => {
-    try {
-      handler(...args);
-    } catch {
-      // ignore this!
-    }
-  };
-};
-
-export class EventEmitter<EventTypes extends string = string> implements IEventEmitter<EventTypes> {
-  #emitter: EE;
-  #mapper: Map<HandlerFn, HandlerFn>;
-
-  constructor() {
-    this.#emitter = new EE();
-    this.#mapper = new Map();
-  }
-
-  emit(event: EventTypes, ...args: any[]): boolean {
-    return this.#emitter.emit(event, ...args);
-  }
-
-  protected clearEvents() {
-    this.#emitter.removeAllListeners();
-    this.#mapper.clear();
-  }
-
-  public on(event: EventTypes, handler: HandlerFn): Unsub {
-    const wrapper = handlerWrapper(handler);
-    this.#mapper.set(handler, wrapper);
-    this.#emitter.on(event, wrapper);
-
-    return () => {
-      this.off(event, handler);
-    };
-  }
-
-  public once(event: EventTypes, handler: HandlerFn): Unsub {
-    const wrapper = handlerWrapper(handler);
-    this.#mapper.set(handler, wrapper);
-
-    this.#emitter.once(event, wrapper);
-
-    return () => {
-      this.off(event, handler);
-    };
-  }
-
-  public off(event: EventTypes, handler?: HandlerFn): this {
-    if (handler) {
-      const wrapper = this.#mapper.get(handler);
-      if (!wrapper) return this;
-
-      this.#emitter.off(event, wrapper);
-      this.#mapper.delete(handler);
-    } else {
-      this.#emitter.off(event);
-    }
-
-    return this;
-  }
 }
 
 export default new EventEmitter();
