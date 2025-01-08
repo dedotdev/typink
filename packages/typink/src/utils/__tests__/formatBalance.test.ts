6cd4d859f625a28e8b17cd0095d25c655691b817
1cedrus <aowiej23s@gmail.com>
2025-01-08 16:47:18 +0700

    refactor: refactoring and renaming

--- packages/typink/src/utils/__tests__/formatBalance.test.ts
+++ packages/typink/src/utils/__tests__/formatBalance.test.ts
@@ -1,40 +1,30 @@
 import { describe, it, expect } from 'vitest';
-import { formatBalance } from '../formatBalance';
+import { formatBalance } from '../formatBalance.js';
+import { TypinkError } from '../errors.js';
 
 describe('formatBalance', () => {
-  
-  it('case 1', () => {
+  it('should work probably', () => {
     expect(formatBalance(1e12, { decimals: 12, symbol: 'AZERO' })).toEqual('1 AZERO');
-  });
-
-  it('case 2', () => {
     expect(formatBalance(1e12, { decimals: 13, symbol: 'AZERO' })).toEqual('0.1 AZERO');
-  });
-
-  it('case 3', () => {
-    expect(formatBalance(1e12, { decimals: 13 })).toEqual('0.1');
-  });
-
-  it('case 4', () => {
     expect(formatBalance(12_023_172_837_123, { decimals: 12 })).toEqual('12.023172837123');
-  });
-
-  it('case 5', () => {
     expect(formatBalance('1', { decimals: 12 })).toEqual('0.000000000001');
+    expect(formatBalance(1e12, { decimals: 13 })).toEqual('0.1');
+    expect(formatBalance(-10000, { decimals: 4 })).toEqual('-1');
+    expect(formatBalance('-10200', { decimals: 4 })).toEqual('-1.02');
+    expect(formatBalance(-1e12, { decimals: 12, symbol: 'AZERO' })).toEqual('-1 AZERO');
   });
 
-  // Unhandled case
-  /*
-  it('case 6', () => {
-    expect(formatBalance('1.000000000001', { decimals: 12 })).toEqual('1.000000000001');
-  });
-  */
-
-  it('case 7', () => {
-    expect(() => formatBalance('1', { decimals: 12.2, symbol: 'AZERO' })).toThrow(new Error('Invalid decimals'));
+  it('should throw error if input has bad chars', () => {
+    expect(() => formatBalance('1.000000000001', { decimals: 12 })).toThrow(
+      new TypinkError('Invalid value at position 1, bigint was expected'),
+    );
+    expect(() => formatBalance('1,2', { decimals: 12, symbol: 'AZERO' })).toThrow(
+      new TypinkError('Invalid value at position 1, bigint was expected'),
+    );
   });
 
-  it('case 8', () => {
-    expect(() => formatBalance('1,2', { decimals: 12, symbol: 'AZERO' })).toThrow(new Error('Invalid value at position 1'));
+  it('should throw error if decimals is invalid', () => {
+    expect(() => formatBalance('1', { decimals: 12.2, symbol: 'AZERO' })).toThrow(new TypinkError('Invalid decimals'));
+    expect(() => formatBalance('1', { decimals: -12, symbol: 'AZERO' })).toThrow(new TypinkError('Invalid decimals'));
   });
 });
--- packages/typink/src/utils/formatBalance.ts
+++ packages/typink/src/utils/formatBalance.ts
@@ -1,3 +1,5 @@
+import { TypinkError } from './errors.js';
+
 export interface FormatBalanceOptions {
   decimals: number;
   symbol?: string;
@@ -6,31 +8,27 @@ export interface FormatBalanceOptions {
 export function formatBalance(value: number | bigint | string, options: FormatBalanceOptions): string {
   const { decimals, symbol } = options;
 
-  // Do it with string
   let valueStr = value.toString();
 
-  // Verify decimals in an integer
   if (!Number.isInteger(decimals) || decimals < 0) {
-    throw new Error('Invalid decimals');
+    throw new TypinkError('Invalid decimals');
   }
 
-  // Make sure the valueStr does have any bad characters
+  const isNegative = valueStr.at(0) === '-';
+  if (isNegative) {
+    valueStr = valueStr.slice(1);
+  }
 
-  const badChars = valueStr.match(/[^0-9.]/);
+  const badChars = valueStr.match(/[^0-9]/);
   if (badChars) {
- k  throw new Error(`Invalid value at position ${badChars.index ?? 0}`);
+    throw new TypinkError(`Invalid value at position ${badChars.index ?? 0}, bigint was expected`);
   }
-
-  // Should we handle negative values? and how?
-  // Should we handle the case where the value is a string with a decimal point?
 
   const tmpStr = valueStr.padStart(decimals, '0').padEnd(decimals, '0');
-  const wholePart = tmpStr.slice(0, tmpStr.length - decimals).padStart(1, '0'); 
+
+  // If wholePart is empty, pad it with 0
+  const wholePart = tmpStr.slice(0, tmpStr.length - decimals).padStart(1, '0');
   const decimalPart = tmpStr.slice(tmpStr.length - decimals).replace(/0+$/, '');
 
-  if (symbol) {
-    return `${wholePart}${decimalPart ? `.${decimalPart}` : ''} ${symbol}`;
-  }
-
-  return `${wholePart}${decimalPart ? `.${decimalPart}` : ''}`;
+  return `${isNegative ? '-' : ''}${wholePart}${decimalPart ? `.${decimalPart}` : ''}${symbol ? ` ${symbol}` : ''}`;
 }
