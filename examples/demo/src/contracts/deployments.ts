108c3b4ceb4174234713a81fdfa9d3ec5ef1e80d
1cedrus <aowiej23s@gmail.com>
2025-01-25 21:39:03 +0700

    Deploy contract in astar and alephzero

--- examples/demo/src/contracts/deployments.ts
+++ examples/demo/src/contracts/deployments.ts
@@ -1,4 +1,4 @@
-import { alephZeroTestnet, ContractDeployment, popTestnet } from 'typink';
+import { alephZeroTestnet, ContractDeployment, popTestnet, alephZero, astar } from 'typink';
 import greeterMetadata from './artifacts/greeter/greeter.json';
 import psp22Metadata from './artifacts/psp22/psp22.json';
 
@@ -22,6 +22,18 @@ export const greeterDeployments: ContractDeployment[] = [
     network: alephZeroTestnet.id,
     address: '5CDia8Y46K7CbD2vLej2SjrvxpfcbrLVqK2He3pTJod2Eyik',
   },
+  {
+    id: ContractId.GREETER,
+    metadata: greeterMetadata as any,
+    network: alephZero.id,
+    address: '5CYZtKBxuva33JREQkbeaE4ed2niWb1ijS4pgXbFD61yZti1',
+  },
+  {
+    id: ContractId.GREETER,
+    metadata: greeterMetadata as any,
+    network: astar.id,
+    address: 'WejJavPYsGgcY8Dr5KQSJrTssxUh5EbeYiCfdddeo5aTbse',
+  },
 ];
 
 export const psp22Deployments: ContractDeployment[] = [
@@ -36,6 +48,18 @@ export const psp22Deployments: ContractDeployment[] = [
     metadata: psp22Metadata as any,
     network: alephZeroTestnet.id,
     address: '5G5moUCkx5E2TD3CcRWvweg7rpCLngRmwukuKdaohvfBBmXr',
+  },
+  {
+    id: ContractId.PSP22,
+    metadata: psp22Metadata as any,
+    network: alephZero.id,
+    address: '5EkDPuyLdubc4uUmEhzMFRtcNtmxSoUecfcc9wWLUR7ZFXbb',
+  },
+  {
+    id: ContractId.PSP22,
+    metadata: psp22Metadata as any,
+    network: astar.id,
+    address: 'YFhgALp2qPtPe1pTFereqqB4RUXKzVM7YtCYfqQX412GWHr',
   },
 ];
 
