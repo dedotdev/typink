d7ed095e82502b958bf44a2b4a0442839a2bbdc7
Thang X. Vu <thang@coongcrafts.io>
2025-07-27 12:50:01 +0700

    update networks

--- packages/typink/src/networks/mainnet.ts
+++ packages/typink/src/networks/mainnet.ts
@@ -31,4 +31,16 @@ export const shiden: NetworkInfo = {
   subscanUrl: 'https://shiden.subscan.io',
 };
 
-// TODO kusama asset hub
+export const kusamaAssetHub: NetworkInfo = {
+  id: 'kusama_asset_hub',
+  name: 'Kusama AssetHub',
+  logo: 'https://assethub-kusama.subscan.io/_next/image?url=%2Fchains%2Fassethub-kusama%2Flogo-mini.png&w=256&q=75',
+  providers: [
+    'wss://rpc.ibp.network/kusama', // --
+    'wss://kusama-rpc.n.dwellir.com',
+    'wss://kusama.api.onfinality.io/public-ws',
+  ],
+  symbol: 'KSM',
+  decimals: 12,
+  subscanUrl: 'https://assethub-kusama.subscan.io',
+};
