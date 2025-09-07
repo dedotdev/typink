4e8d57ffd0cf06338acf5b744125029c4011270b
1cedrus <aowiej23s@gmail.com>
2025-09-07 10:33:05 +0700

    Update templates .eslintrc.js

--- packages/create-typink/templates/vite/.eslintrc.cjs
+++ packages/create-typink/templates/vite/.eslintrc.cjs
@@ -19,12 +19,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "react-hooks/exhaustive-deps": "off"
+    'react-refresh/only-export-components': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react-hooks/exhaustive-deps': 'off'
   },
 }
