2ecdf455410ff6e3413729e10bc04b2e2d2dc70c
Thang X. Vu <thang@coongcrafts.io>
2025-09-18 10:58:02 +0700

    clean up cli options

--- packages/create-typink/src/types.ts
+++ packages/create-typink/src/types.ts
@@ -7,9 +7,7 @@ export type BaseOptions = {
   projectName: string | null;
   skipInstall: boolean;
   inkVersion: InkVersion | null;
-  walletConnector: WalletConnector | null;
   networks: string[] | null;
-  ui: UI | null;
   template: Template | null;
   // This option will be determined by the user agent instead of being set by the user
   pkgManager: PkgManagerInfo;
@@ -23,24 +21,8 @@ export type RawOptions = {
 
 export type Options = BaseOptions & RawOptions;
 
-// Template naming convention: {inkVersion}-{walletConnector}-{nextjs|vite}
-// inkVersion: legacy | v6
-// walletConnector: typink | subconnectv2 | talisman
-// ui: nextjs | vite
-export const TEMPLATES = [
-  'legacy-typink-vite',
-  'legacy-subconnectv2-vite',
-  'legacy-talisman-vite',
-  'legacy-typink-nextjs',
-  'legacy-subconnectv2-nextjs',
-  'legacy-talisman-nextjs',
-  'v6-typink-vite',
-  'v6-subconnectv2-vite',
-  'v6-talisman-vite',
-  'v6-typink-nextjs',
-  'v6-subconnectv2-nextjs',
-  'v6-talisman-nextjs',
-] as const;
+export const TEMPLATES = ['legacy-nextjs', 'v6-nextjs'] as const;
+
 export type Template = (typeof TEMPLATES)[number];
 
 export enum InkVersion {
@@ -58,43 +40,6 @@ export const INK_VERSIONS_CHOICES = [
     name: 'v5 & v4 (WASM, pallet-contracts)',
     value: InkVersion.InkLegacy,
     short: 'v5 & v4',
-  },
-] as const;
-
-export enum WalletConnector {
-  Typink = 'typink',
-  SubConnectV2 = 'subconnectv2',
-  Talisman = 'talisman',
-}
-
-export const WALLET_CONNECTORS_CHOICES = [
-  {
-    name: 'Typink',
-    value: WalletConnector.Typink,
-  },
-  {
-    name: 'SubConnect V2',
-    value: WalletConnector.SubConnectV2,
-  },
-  {
-    name: 'Talisman Connect',
-    value: WalletConnector.Talisman,
-  },
-] as const;
-
-export enum UI {
-  Vite = 'vite',
-  NextJS = 'nextjs',
-}
-
-export const UI_CHOICES = [
-  {
-    name: 'Vite + Charka UI',
-    value: UI.Vite,
-  },
-  {
-    name: 'Next.JS + shadcn/ui',
-    value: UI.NextJS,
   },
 ] as const;
 
