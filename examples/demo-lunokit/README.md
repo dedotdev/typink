803b27bc76d99ba1f4143c185e09c32da6ed7cd9
wbh1328551759 <1328551759@qq.com>
2025-10-23 13:21:33 +0800

    feat: add example for LunoKit integration

---
+++ examples/demo-lunokit/README.md
@@ -0,0 +1,10 @@
+# Example Dapp
+
+- Start the application by running:
+```shell
+# From examples/demo-lunokit folder
+yarn dev
+
+# From the project root folder
+yarn workspace demo-lunokit dev
+```
---
+++ examples/demo-lunokit/src/contracts/deployments.ts
@@ -0,0 +1,40 @@
+import { alephZeroTestnet, ContractDeployment, popTestnet } from 'typink';
+import greeterMetadata from './artifacts/greeter/greeter.json';
+import psp22Metadata from './artifacts/psp22/psp22.json';
+
+export enum ContractId {
+  GREETER = 'greeter',
+  PSP22 = 'psp22',
+}
+
+export const greeterDeployments: ContractDeployment[] = [
+  {
+    id: ContractId.GREETER,
+    metadata: greeterMetadata,
+    network: popTestnet.id,
+    address: '5HJ2XLhBuoLkoJT5G2MfMWVpsybUtcqRGWe29Fo26JVvDCZG',
+  },
+  {
+    id: ContractId.GREETER,
+    metadata: greeterMetadata as any,
+    network: alephZeroTestnet.id,
+    address: '5CDia8Y46K7CbD2vLej2SjrvxpfcbrLVqK2He3pTJod2Eyik',
+  },
+];
+
+export const psp22Deployments: ContractDeployment[] = [
+  {
+    id: ContractId.PSP22,
+    metadata: psp22Metadata as any,
+    network: popTestnet.id,
+    address: '16119BccKAfWwbt4TCNvfLBDuRWHSeFozJELEcxFPVd11hnt',
+  },
+  {
+    id: ContractId.PSP22,
+    metadata: psp22Metadata as any,
+    network: alephZeroTestnet.id,
+    address: '5G5moUCkx5E2TD3CcRWvweg7rpCLngRmwukuKdaohvfBBmXr',
+  },
+];
+
+export const deployments = [...greeterDeployments, ...psp22Deployments];
